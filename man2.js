c9319d8f4b7d7ddd75bd89b4464d5f0a3b0b39e4
naveen <naveen.kumar.met14@itbhu.ac.in>
2021-04-02 20:52:39 +0530

    load script

---
+++ man2.js
@@ -0,0 +1,25 @@
+function injectScript(src) {
+  return new Promise((resolve, reject) => {
+    const script = document.createElement('script');
+    script.src = src;
+    script.onload = () => resolve(src);
+    script.onerror = () => reject(new Error(`unable to load script: ${src}`));
+    document.head.appendChild(script);
+  });
+}
+
+injectScript('http://localhost:5500/man1.js')
+  .then(() => {
+    console.log('Script loaded!');
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
+injectScript('http://localhost:5500/man.js')
+  .then(() => {
+    console.log('Script loaded!');
+  })
+  .catch((error) => {
+    console.error(error);
+  });
